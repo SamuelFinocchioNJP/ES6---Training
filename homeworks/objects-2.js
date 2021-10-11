2d0e5996d5ea48e5b3f97d9e040303be09829fe5
Samuel Finocchio <informaticage@Samuels-MacBook-Pro.local>
2021-10-11 12:34:05 +0200

    http requet template

--- homeworks/objects-2.js
+++ homeworks/objects-2.js
@@ -1,2 +1,12 @@
 // https://5f9fef6ce21bab0016dfc996.mockapi.io/api/v1/shop
 // https://5f9fef6ce21bab0016dfc996.mockapi.io/api/v1/shop/1/product
+
+const makeHttpCall = async function (url) {
+  return await (await fetch(url)).json();
+};
+
+(async () => {
+  const dati = await makeHttpCall(
+    "https://5f9fef6ce21bab0016dfc996.mockapi.io/api/v1/shop"
+  );
+})();
