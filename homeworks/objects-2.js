0518fef5106c3b29a1a116941b91023a2808712a
Samuel Finocchio <informaticage@Samuels-MacBook-Pro.local>
2021-10-11 14:59:32 +0200

    Added solution to problem 2

--- homeworks/objects-2.js
+++ homeworks/objects-2.js
@@ -5,8 +5,32 @@ const makeHttpCall = async function (url) {
   return await (await fetch(url)).json();
 };
 
+const filterShops = (shops, price, offsett = 0, colors = []) => {
+  const [minPrice, maxPrice] = [
+    Math.max(price - offsett, 0),
+    Math.max(price + offsett, 0),
+  ];
+
+  return shops.filter((shop) =>
+    shop.products.some(
+      (product) =>
+        product.price >= minPrice &&
+        product.price <= maxPrice &&
+        product.color.some((_color) => colors.includes(_color.name))
+    )
+  );
+};
+
 (async () => {
-  const dati = await makeHttpCall(
+  const result = await makeHttpCall(
     "https://5f9fef6ce21bab0016dfc996.mockapi.io/api/v1/shop"
   );
+
+  for (let shop of result) {
+    shop.products = await makeHttpCall(
+      `https://5f9fef6ce21bab0016dfc996.mockapi.io/api/v1/shop/${shop.id}/product`
+    );
+  }
+
+  console.log(filterShops(result, 500, 100, ["cyan", "silver"]));
 })();
